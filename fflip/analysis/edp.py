1fcb1fc755a4aa7b906e177707208e3ed18b4528
anthony-pane <anthony.joel.pane@gmail.com>
2022-10-03 20:34:27 -0400

    test dhh

--- fflip/analysis/edp.py
+++ fflip/analysis/edp.py
@@ -245,11 +245,11 @@ class MembraneDhh(BinEdgeUpdater):
                 normalized[fr], bins=self.num_bins, range=(0, 1),
                 weights=self.masses
             )  # this is !much! faster than manual bins
-            for x,y in zip(histogram[0],histogram[1]):
-                print(x,y)
-            return
             distrib = np.array([histogram[0]])[0]
             data = smooth_edp(distrib, 12)
+            for x,y in zip(histogram[0],data):
+                print(x,y)
+            return
             rho_max_n = np.amax(data[:int(self.num_bins/2)])
             n_position = np.where(data==rho_max_n)
             rho_max_p = np.amax(data[int(self.num_bins/2):])
